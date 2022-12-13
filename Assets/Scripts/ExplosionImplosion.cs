f080616c77433aac6df2aa20ee5b7f7b3d2ce20c
Ace <stacy.felde@gmail.com>
2022-12-12 20:00:45 -0500

    renamed action to source

--- Assets/Scripts/ExplosionImplosion.cs
+++ Assets/Scripts/ExplosionImplosion.cs
@@ -8,17 +8,17 @@ public class ExplosionImplosion : MonoBehaviour
     [SerializeField] int pushbackAmount;
     [SerializeField] bool push;
     [SerializeField] ParticleSystem hitEffect;
-    [SerializeField] GameObject action;
+    [SerializeField] GameObject source;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (push)
-                GameManager.instance.playerScript.PushbackInput((other.transform.position - action.transform.position) * pushbackAmount);
+                GameManager.instance.playerScript.PushbackInput((other.transform.position - source.transform.position) * pushbackAmount);
             else
-                GameManager.instance.playerScript.PushbackInput((action.transform.position - other.transform.position) * pushbackAmount);
-            Instantiate(hitEffect, action.transform.position, action.transform.rotation);
+                GameManager.instance.playerScript.PushbackInput((source.transform.position - other.transform.position) * pushbackAmount);
+            Instantiate(hitEffect, source.transform.position, source.transform.rotation);
         }
         Destroy(gameObject);
     }
