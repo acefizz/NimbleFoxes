e7a1b0776ef67fb13477b7d4225a14ee7b9a3a9b
Fernando Vicente Francisco <dawolf820@gmail.com>
2022-12-14 20:31:45 -0500

    script is done. Just need to use an object to apply the script.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
+    [SerializeField] int enemiesToSpawn;
     [SerializeField] int timer;
     [SerializeField] Transform spawnPos;
    
     bool isSpawning;
-    bool playerInRange; 
+    bool playerInRange;
+    int enemiesSpawned;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,7 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerInRange && !isSpawning)
+        if (playerInRange && !isSpawning && enemiesSpawned < enemiesToSpawn)
         {
             StartCoroutine(spawn());
         }
@@ -36,9 +38,8 @@ public class Spawner : MonoBehaviour
     IEnumerator spawn()
     {
         isSpawning = true;
-       
         Instantiate(enemy, spawnPos.position, enemy.transform.rotation);
-       
+        enemiesSpawned++;
         yield return new WaitForSeconds(timer);    
         isSpawning = false;
     }
