a6c4cd5cb92910548cbf6d78fd84bb5976a9e888
Fernando Vicente Francisco <dawolf820@gmail.com>
2022-12-14 20:24:28 -0500

    working on script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] GameObject enemy;
+    [SerializeField] int timer;
+    [SerializeField] Transform spawnPos;
+   
+    bool isSpawning;
+    bool playerInRange; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,26 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange && !isSpawning)
+        {
+            StartCoroutine(spawn());
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+        }
+    }
+    IEnumerator spawn()
+    {
+        isSpawning = true;
+       
+        Instantiate(enemy, spawnPos.position, enemy.transform.rotation);
+       
+        yield return new WaitForSeconds(timer);    
+        isSpawning = false;
     }
 }
